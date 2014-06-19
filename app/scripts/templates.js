e818f6be08a6526ba29a4ee9e14f54c4931773ad
Philip Radvan <pradvan@hotmail.com>
2014-06-19 16:26:12 -0400

    making css nicer, added modal template

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -17,3 +17,12 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
   return "<div class=\"panel panel-default\">\n	<div class=\"panel-body\">\n		<h2>Home</h2>\n		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat lectus et nulla tincidunt fringilla. Aenean enim tellus, convallis ornare augue et, luctus rhoncus ligula.</p>\n	</div>\n</div>";
   });
+
+this["JST"]["app/templates/modal.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<div class=\"panel panel-default\">\n	<div class=\"panel-body\">\n		<h2>Home</h2>\n		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat lectus et nulla tincidunt fringilla. Aenean enim tellus, convallis ornare augue et, luctus rhoncus ligula.</p>\n	</div>\n</div>";
+  });
